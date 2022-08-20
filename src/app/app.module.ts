f6abe744b3952e0b36866ca32c977a00aa59983c
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-20 20:28:24 -0300

    feat: created home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,6 +27,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
+import { HomeComponent } from './components/home/home.component';
 
 
 
@@ -35,6 +36,7 @@ import { NavComponent } from './components/nav/nav.component';
   declarations: [
     AppComponent,
     NavComponent,
+    HomeComponent,
 
   ],
   imports: [
