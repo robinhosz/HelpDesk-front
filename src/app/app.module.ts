1f4f5ab312601bbd3ff555f501dcf21ffa6b2dd7
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-20 20:54:49 -0300

    feat: tecnico list component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TecnicoListComponent } from './components/tecnico/tecnico-list/tecnico-list.component';
 
 
 
@@ -39,6 +40,7 @@ import { HeaderComponent } from './components/header/header.component';
     NavComponent,
     HomeComponent,
     HeaderComponent,
+    TecnicoListComponent,
 
   ],
   imports: [
