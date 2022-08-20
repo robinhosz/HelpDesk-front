b214669faff7ca009615c4dfeb741f5d763548c8
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-20 20:45:33 -0300

    feat: header component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,7 @@ import { MatListModule } from '@angular/material/list';
 import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 
@@ -37,6 +38,7 @@ import { HomeComponent } from './components/home/home.component';
     AppComponent,
     NavComponent,
     HomeComponent,
+    HeaderComponent,
 
   ],
   imports: [
