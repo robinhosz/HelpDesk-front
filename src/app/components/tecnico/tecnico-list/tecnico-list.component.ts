baae070f95884824138db48420cd1e98cbe067de
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-20 21:14:01 -0300

    feat: final styles tecnico list

--- src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
+++ src/app/components/tecnico/tecnico-list/tecnico-list.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { Tecnico } from 'src/app/models/tecnico';
 
 @Component({
   selector: 'app-tecnico-list',
   templateUrl: './tecnico-list.component.html',
   styleUrls: ['./tecnico-list.component.css']
 })
-export class TecnicoListComponent implements OnInit {
+export class TecnicoListComponent implements AfterViewInit {
+  
+  ELEMENT_DATA: Tecnico[] = [
+    {
+      id: 1,
+      nome: 'Jose',
+      cpf: '123',
+      email: 'jose@gmail.com',
+      senha: '1234',
+      perfis: ['0'],
+      dataCriacao: '15/08/2022'
+    }
+  ]
+
+  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol', 'acoes'];
+  dataSource = new MatTableDataSource<Tecnico>(this.ELEMENT_DATA);
+
+  @ViewChild(MatPaginator) paginator: MatPaginator
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
   }
 
 }
