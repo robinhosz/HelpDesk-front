1ba7e9b224d41f22b2c84d863140f3427ec94b55
José Robson <joserobsonsiqueira23@hotmail.com>
2022-08-20 22:21:10 -0300

    refact: implements login auth

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth/auth.guard';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { NavComponent } from './components/nav/nav.component';
@@ -9,7 +10,7 @@ const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {
     path: '', 
-    component: NavComponent, children: [
+    component: NavComponent, canActivate: [AuthGuard], children: [
       {path: 'home', component: HomeComponent},
       {path: 'tecnicos', component: TecnicoListComponent}
     ]
